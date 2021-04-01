341e5e6589bf428798825d8ff170aadf762f3b71
Newturno <pongkorn_set@hotmail.com>
2021-04-02 00:37:37 +0700

    Lab10

---
+++ lab10/lecture/test1.py
@@ -0,0 +1,6 @@
+from tqdm.contrib.discord import tqdm, trange
+
+iter = range(1000)
+
+for i in tqdm(iter, token='ODI3MTkyMDYyNzU0ODE2MDIy.YGXcpA.py4WQmZsv0HPu5eh2se117TNRzk', channel_id='827196085738536971'):
+    print(i)
